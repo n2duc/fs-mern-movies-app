5d8472a5802e822ea9473c57b4b509e841db62d6
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2023-09-30 21:41:04 +0700

    setup redux ⚙️

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { Provider } from "react-redux";
+import store from './redux/store';
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,7 +10,7 @@ import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
   // </React.StrictMode>,
